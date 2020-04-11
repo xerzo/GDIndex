ae0ad5af31b576abfa6e0b36e38a7a80a2aabc64
Lazy Meister <32761826+xerzo@users.noreply.github.com>
2020-04-11 20:58:40 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -37,7 +37,7 @@
 					text
 					class="text-none hidden-sm-and-down"
 					tag="a"
-					href="https://github.com/maple3142/GDIndexxxxxx"
+					href="https://github.com/maple3142/GDIndexxxx"
 					target="_blank"
 				>
 					<v-icon>mdi-github-circle</v-icon>&nbsp;GitHuxb</v-btn
